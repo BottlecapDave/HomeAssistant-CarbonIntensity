723d18a841a0a7518245292e99e272cd7e2c638a
BottlecapDave <me@davidskendall.co.uk>
2024-09-22 11:48:21 +0100

    chore: Updated build commands to match other repos

---
+++ .build/createGithubRelease.ts
@@ -0,0 +1,64 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const bodySuffix = "---\nEnjoying the integration? Why not make a one time or monthly [GitHub sponsorship](https://github.com/sponsors/bottlecapdave)?"
+
+function getMinimumHomeAssistantVersion() {
+  const hacsFilePath = join(__dirname, '../hacs.json');
+  const buffer = readFileSync(hacsFilePath);
+  const content = JSON.parse(buffer.toString());
+  return content.homeassistant;
+}
+
+async function createGithubRelease(githubToken: string, githubOwnerRepo: string, tag: string, notes: string) {
+  if (!githubToken) {
+    throw new Error('Github token not specified');
+  }
+
+  if (!githubOwnerRepo) {
+    throw new Error('Github owner/repo not specified');
+  }
+
+  if (!tag) {
+    throw new Error('Tag not specified');
+  }
+
+  if (!notes) {
+    throw new Error('Notes not specified');
+  }
+
+  console.log(`Publishing ${tag} release to ${githubOwnerRepo}`);
+
+  const body = JSON.stringify({
+    tag_name: tag,
+    name: tag,
+    body: notes,
+    draft: false,
+    prerelease:false
+  });
+
+  const response = await fetch(
+    `https://api.github.com/repos/${githubOwnerRepo}/releases`,
+    { 
+      method: 'POST',
+      headers: {
+        "Accept": "application/vnd.github+json",
+        "Authorization": `Bearer ${githubToken}`,
+        "X-GitHub-Api-Version": "2022-11-28" 
+      },
+      body
+    }
+  );
+
+  if (response.status >= 300) {
+    throw new Error(response.statusText);
+  }
+}
+
+const minimumHAVersionNote = `\n**Minimum HA Version**: ${getMinimumHomeAssistantVersion()}\n\n`;
+createGithubRelease(
+  process.env.GITHUB_TOKEN,
+  process.env.GITHUB_REPOSITORY,
+  process.argv[2],
+  `${process.argv[3]}\n${minimumHAVersionNote}${bodySuffix}`
+).then(() => console.log('Success'));
---
+++ .build/updateVersion.ts
@@ -0,0 +1,32 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+const manifestFilePath = join(__dirname, '../custom_components/octopus_energy/manifest.json');
+const constantFilePath = join(__dirname, '../custom_components/octopus_energy/const.py');
+
+function updateManifestVersion(version: string) {
+  const buffer = readFileSync(manifestFilePath);
+  const content = JSON.parse(buffer.toString());
+  content.version = version;
+
+  writeFileSync(manifestFilePath, JSON.stringify(content, null, 2));
+  console.log(`Updated manifest version to '${version}'`);
+}
+
+function updateVersionConstant(version: string) {
+  const buffer = readFileSync(constantFilePath);
+  const oldContent = buffer.toString();
+
+  const versionRegex = /INTEGRATION_VERSION = \"[^\"]+\"/g
+  const newContent = oldContent.replace(versionRegex, `INTEGRATION_VERSION = \"${version}\"`);
+
+  if (oldContent != newContent) {
+    writeFileSync(constantFilePath, newContent);
+    console.log(`Updated constant version to '${version}'`);
+  } else {
+    console.log(`Failed to update constant version to '${version}'`);
+  }
+}
+
+updateManifestVersion(process.argv[2]);
+updateVersionConstant(process.argv[2]);
